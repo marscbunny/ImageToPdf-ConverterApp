ecc8a94a9ef0b0c4c5510e161d62b4e365d3f0db
navneet <navneetpalgithub@gmail.com>
2024-05-28 16:00:33 +0530

    Marsyy

---
+++ app/src/main/java/com/example/imagetopdf_app/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.imagetopdf_app
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.imagetopdf_app.ui.theme.ImageToPdfAppTheme
+
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
+        super.onCreate(savedInstanceState)
+        setContent {
+            ImageToPdfAppTheme {
+
+            }
+        }
+    }
+}
